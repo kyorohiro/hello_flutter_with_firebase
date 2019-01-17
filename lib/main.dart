afab5c0f609128699b24ae5562277ace07dda72c
kyorohiro <kyorohiro@gmail.com>
2019-01-18 01:53:26 +0900

    flutter firebase tutorial

---
+++ lib/main.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart' as fb;
+
+void main() => runApp(MyApp());
+
+final dummySnapshot = [
+  {"name": "Filip", "votes": 15},
+  {"name": "Abraham", "votes": 14},
+  {"name": "Richard", "votes": 11},
+  {"name": "Ike", "votes": 10},
+  {"name": "Justin", "votes": 1},
+];
+
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Boby Names',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Baby Names'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: _buildBody(context)
+    );
+  }
+
+  Widget _buildBody(BuildContext context) {
+    return _buildList(context, dummySnapshot);
+  }
+
+  Widget _buildList(BuildContext, List<Map> snapshot) {
+    return ListView(
+      padding: const EdgeInsets.only(top: 20.0),
+      children: snapshot.map((data) => _buildListItem(context, data)).toList(),
+    );
+  }
+
+  Widget _buildListItem(BuildContext context,Map data) {
+    final record = Record.fromMap(data);
+    
+    return Padding(
+        key: ValueKey(record.name),
+        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+        child: Container(
+          decoration:  BoxDecoration(
+            border: Border.all(color: Colors.grey),
+            borderRadius: BorderRadius.circular(5.0),
+          ),
+          child: ListTile(
+            title: Text(record.name),
+            trailing: Text(record.votes.toString()),
+            onTap: ()=> print(record),
+          ),
+        ),
+    );
+  }
+
+}
+
+
+class Record {
+  final String name;
+  final int votes;
+  final fb.DocumentReference reference;
+
+  Record.fromMap(Map<String, dynamic> map, {this.reference})
+      : this.name = map["name"],
+        this.votes = map["votes"];
+
+  Record.fromSnapshot(fb.DocumentSnapshot snapshot)
+  : this.fromMap(snapshot.data, reference: snapshot.reference);
+
+  @override
+  String toString() => "Record<$name:$votes>";
+
+}
